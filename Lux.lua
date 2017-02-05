c45e403de972e57d5dc168d5dd8c2ec7748e731b
Janilson Santos <janilssonn@hotmail.com>
2017-02-04 22:22:11 -0200

    ReCode

--- Lux.lua
+++ Lux.lua
@@ -2,7 +2,7 @@
 if GetObjectName(GetMyHero()) ~= "Lux" then return end
 
 --          [[ Updater ]]
-local Ver = "0.1"
+--[[local Ver = "0.1"
 
 local function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
@@ -14,7 +14,7 @@ local function AutoUpdate(data)
     end	
 end
 
-GetWebResultAsync("https://raw.githubusercontent.com/janilssonn/GoS/master/Version/Lux.version", AutoUpdate)
+GetWebResultAsync("https://raw.githubusercontent.com/janilssonn/GoS/master/Version/Lux.version", AutoUpdate)]]
 
 --          [[ Lib ]]
 require ("OpenPredict")
@@ -28,8 +28,8 @@ LuxMenu:SubMenu("Combo", "Combo Settings")
 LuxMenu.Combo:Boolean("Q", "Use Q", true)
 LuxMenu.Combo:Boolean("W", "Use W", true)
 LuxMenu.Combo:Boolean("WA", "Use W on Ally", false)
-LuxMenu.Combo:Slider("WM", "Use W on HP", 40, 1, 100, 1)
-LuxMenu.Combo:Slider("WMA", "No Options here", 1, 1, 1, 1)
+LuxMenu.Combo:Slider("WM", "Use W on HP", 50, 1, 100, 1)
+LuxMenu.Combo:Slider("WMA", "No Options here", 1, 1, 5, 1)
 LuxMenu.Combo:Boolean("E", "Use E", true)
 
 --          [[ Harass ]]
@@ -48,7 +48,6 @@ LuxMenu.Farm:Slider("Mana", "Min. Mana", 40, 0, 100, 1)
 LuxMenu:SubMenu("JG", "Jungle Settings")
 LuxMenu.JG:Boolean("Q", "Use Q", true)
 LuxMenu.JG:Boolean("E", "Use E", true)
-LuxMenu.JG:Slider("Mana", "Min. Mana", 40, 0, 100, 1)
 
 --          [[ KillSteal ]]
 LuxMenu:SubMenu("Ks", "KillSteal Settings")
@@ -63,10 +62,12 @@ LuxMenu.Draw:Boolean("W", "Draw W", false)
 LuxMenu.Draw:Boolean("E", "Draw E", false)
 
 --          [[ Spell ]]
-local LuxQ = {delay = 0.25, range = 1300, width = 80, speed = 1200}
-local LuxW = {delay = 0.25, range = 1075, width = 150, speed = 1200}
-local LuxE = {delay = 0.25, range = 1100, radius = 275, speed = 1400}
-local LuxR = {delay = 1.35, range = 3340, width = 190, speed = math.huge}
+local Spells = {
+ Q = {range = 1175, delay = 0.25, speed = 1200, width = 70},
+ W = {range = 1075, delay = 0.25, speed = 1300, width = 95},
+ E = {range = 1100, delay = 0.25, speed = 1300, radius = 330},
+ R = {range = 3340, delay = 1.0, speed = math.huge, width = 190}
+}
 
 --          [[ Orbwalker ]]
 function Mode()
@@ -85,131 +86,147 @@ end
 
 --          [[ Tick ]]
 OnTick(function()
+	KS()
+	target = GetCurrentTarget()
+	         Combo()
+	         Harass()
+	         Farm()
+	    end)  
+--          [[ LuxQ ]]
+function LuxQ()	
+	local QPred = GetPrediction(target, Spells.Q)
+	if QPred.hitChance >0.3 then
+		CastSkillShot(_Q, QPred.castPos)
+	end	
+end   
+--          [[ LuxW ]]
+function LuxW()
+		CastSkillShot(_W, myHero.pos)
+	end	
+--          [[ LuxE ]]
+function LuxE()
+	local EPred = GetCircularAOEPrediction(target, Spells.E)
+	if EPred.hitChance >0.3 then
+		CastSkillShot(_E, EPred.castPos)
+	end	
+end  
+--          [[ LuxR ]]
+function LuxR()
+	local RPred = GetPrediction(target, Spells.R)
+	if RPred.hitChance >0.8 then
+		CastSkillShot(_R, RPred.castPos)
+	end	
+end  
 
-	local target = GetCurrentTarget()
+--          [[ Combo ]]
+
+function Combo()
 	if Mode() == "Combo" then
-		-- [[ Use Q ]]
-		if LuxMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1300) then
-			local QPred = GetPrediction(target, LuxQ)
-			if QPred.hitChance > 0.2 and not QPred:mCollision(2) then
-				CastSkillShot(_Q, QPred.castPos)
+--		[[ Use Q ]]
+		if LuxMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, Spells.Q.range) then
+			LuxQ()
+		end	
+--		[[ Use W ]]
+		if Ready(_W) and GetPercentHP(myHero) <= LuxMenu.Combo.WM:Value() and LuxMenu.Combo.W:Value() and EnemiesAround(myHero, Spells.W.range) >= LuxMenu.Combo.WMA:Value() then
+			LuxW()
 			end
+-- 		[[ Use E ]]
+		if LuxMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, Spells.E.range) then
+			LuxE()
 		end
-		-- [[ Use W ]]
-		if Ready(_W) and GetPercentHP(myHero) <= LuxMenu.Combo.WM:Value() and LuxMenu.Combo.W:Value() and EnemiesAround(myHero, 800) >= LuxMenu.Combo.WMA:Value() then
-				CastSkillShot(_W,myHero.pos)
-			end
-		-- [[ Use E ]]
-		if LuxMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 1100) then
-			local EPred = GetCircularAOEPrediction(target, LuxE)
-			if EPred.hitChance > 0.2 then
-				CastSkillShot(_E, EPred.castPos)
-			end
-		end
+-- 		[[ Use R ]]
+		--[[if LuxMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, Spells.R.range) then
+			LuxR()
+		end]]
 	end
-
+end
+--          [[ Harass ]]
+function Harass()
 	if Mode() == "Harass" then
 		if (myHero.mana/myHero.maxMana >= LuxMenu.Harass.Mana:Value() /100) then
 
-			-- [[ Use Q ]]
-			if LuxMenu.Harass.Q:Value() and Ready(_Q) and ValidTarget(target, 1300) then
-				local QPred = GetPrediction(target, LuxQ)
-				if QPred.hitChance > 0.2 and not QPred:mCollision(2) then	
-					CastSkillShot(_Q, QPred.castPos)
-				end
+-- 			[[ Use Q ]]
+			if LuxMenu.Harass.Q:Value() and Ready(_Q) and ValidTarget(target, Spells.Q.range) then
+				LuxQ()
 			end
 
-			-- [[ Use E ]]
-			if LuxMenu.Harass.E:Value() and Ready(_E) and ValidTarget(target, 1100) then
-				local EPred = GetCircularAOEPrediction(target, LuxE)
-				if EPred.hitChance > 0.2 then
-					CastSkillShot(_E, EPred.castPos)
-				end
+-- 			[[ Use E ]]
+			if LuxMenu.Harass.E:Value() and Ready(_E) and ValidTarget(target, Spells.E.range) then
+				LuxE()
 			end
 		end
 	end
-
+end
+--          [[ LaneClear ]]
+function Farm()
 	if Mode() == "LaneClear" then
 		if (myHero.mana/myHero.maxMana >= LuxMenu.Farm.Mana:Value() /100) then
 			
-			-- [[ Lane ]]
+-- 			[[ Lane ]]
 			for _, minion in pairs(minionManager.objects) do
 				if GetTeam(minion) == MINION_ENEMY then
-					if LuxMenu.Farm.Q:Value() and Ready(_Q) and ValidTarget(minion, 1000) then
-						CastSkillShot(_Q, minion)
+-- 					[[ Use Q ]]
+					if LuxMenu.Farm.Q:Value() and Ready(_Q) and ValidTarget(minion, Spells.Q.range) then
+							CastSkillShot(_Q, minion)
+					    end
+-- 					[[ Use E ]]
+					if LuxMenu.Farm.E:Value() and Ready(_E) and ValidTarget(minion, Spells.E.range) then
+							CastSkillShot(_E, minion)
+						end	
 					end
-				end
-			end
-			for _, minion in pairs(minionManager.objects) do
-				if GetTeam(minion) == MINION_ENEMY then
-					if LuxMenu.Farm.E:Value() and Ready(_E) and ValidTarget(minion, 1000) then
-						CastSkillShot(_E, minion)
-					end
-				end
-			end
+				end	
 
-			-- [[ Jungle ]]
+
+-- 			[[ Jungle ]]
 			for _, mob in pairs(minionManager.objects) do
 				if GetTeam(mob) == MINION_JUNGLE then
-					if LuxMenu.JG.Q:Value() and Ready(_Q) and ValidTarget(mob, 1000) then
-						CastSkillShot(_Q, mob)
-					end
-				end
-			end
-
-			for _, mob in pairs(minionManager.objects) do
-				if GetTeam(mob) == MINION_JUNGLE then
-					if LuxMenu.JG.E:Value() and Ready(_E) and ValidTarget(mob, 1000) then
-						CastSkillShot(_E, mob)
+-- 					[[ Use Q ]]
+					if LuxMenu.JG.Q:Value() and Ready(_Q) and ValidTarget(mob, Spells.Q.range) then
+							CastSkillShot(_Q, mob)
+						end
+-- 					[[ Use E ]]
+					if LuxMenu.JG.E:Value() and Ready(_E) and ValidTarget(mob, Spells.E.range) then
+							CastSkillShot(_E, mob)
+						end	
 					end
 				end
 			end
 		end
 	end
-	
-	-- [[ KillSteal ]]
+
+--          [[ KillSteal ]]
+function KS()
 	for _, enemy in pairs(GetEnemyHeroes()) do
-		-- Q
-		if LuxMenu.Ks.Q:Value() and Ready(_Q) and ValidTarget(enemy, 1300) then
+-- 		[[ Use Q ]]
+		if LuxMenu.Ks.Q:Value() and Ready(_Q) and ValidTarget(enemy, Spells.Q.range) then
 			if GetCurrentHP(enemy) < getdmg("Q", enemy, myHero) then
-				local QPred = GetPrediction(target, LuxQ)
-				if QPred.hitChance > 0.8 and not QPred:mCollision(2) then
-					CastSkillShot(_Q, QPred.castPos)
+				LuxQ()
 				end
 			end
-		end
 
-		-- E
-		if LuxMenu.Ks.E:Value() and Ready(_E) and ValidTarget(enemy, 1100) then
+-- 		[[ Use E ]]
+		if LuxMenu.Ks.E:Value() and Ready(_E) and ValidTarget(enemy, Spells.E.range) then
 			if GetCurrentHP(enemy) < getdmg("E", enemy, myHero) then
-				local EPred = GetCircularAOEPrediction(target, LuxE)
-				if EPred.hitChance > 0.8 then
-					CastSkillShot(_E, EPred.castPos)
+				LuxE()
 				end
 			end
-		end
 
-		-- R
-		if LuxMenu.Ks.R:Value() and Ready(_R) and ValidTarget(enemy, 3340) then
+--		[[ Use R ]]
+		if LuxMenu.Ks.R:Value() and Ready(_R) and ValidTarget(enemy, Spells.R.range) then
 			if GetCurrentHP(enemy) < getdmg("R", enemy, myHero) then
-				local RPred = GetPrediction(target,LuxR)
-				if RPred.hitChance > 0.8 then
-					CastSkillShot(_R, RPred.castPos)
+					LuxR()
 				end
 			end
 		end
 	end
-end)
-       
-
 
 --          [[ Drawings ]]
 OnDraw(function(myHero)
 	local pos = GetOrigin(myHero)
 		-- [[ Draw Q ]]
-	if LuxMenu.Draw.Q:Value() then DrawCircle(pos, 1300, 1, 25, GoS.Red) end
+	if LuxMenu.Draw.Q:Value() then DrawCircle(pos, 1175, 0, 25, GoS.Red) end
 		-- [[ Draw W ]]
-	if LuxMenu.Draw.W:Value() then DrawCircle(pos, 1075, 1, 25, GoS.Blue) end
+	if LuxMenu.Draw.W:Value() then DrawCircle(pos, 1075, 0, 25, GoS.Blue) end
 		-- [[ Draw E ]]
-	if LuxMenu.Draw.E:Value() then DrawCircle(pos, 1100, 1, 25, GoS.Blue) end
+	if LuxMenu.Draw.E:Value() then DrawCircle(pos, 1100, 0, 25, GoS.Green) end
 end)		
